c216aa429b32f9fa724c03b153af29aa3032041a
Keepeuh <keepeuh.calmeuh@gmail.com>
2025-04-29 11:32:37 +0200

    Updates CSV file naming to include URL
    Improves log organization and tracking for multiple URLs.

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def interact_with_slaves(url="https://github.com", graph=False, export=False):
             os.makedirs(logs_dir)
         
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        csv_file_path = os.path.join(logs_dir, f"ping_results_{timestamp}.csv")
+        csv_file_path = os.path.join(logs_dir, f"ping_results_{url}_{timestamp}.csv")
 
         csv_file = open(csv_file_path, mode="w", newline="")
         csv_writer = csv.writer(csv_file)
